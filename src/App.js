292b0d584f7799e076ea5b432a4afa20b332d6c2
Kamil <sikorakamil26>
2020-05-21 20:54:44 +0200

    fixed import

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-import createTheme from "@material-ui/core/styles/createMuiTheme";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 // Components
 import Navbar from "./components/Navbar";
 // Pages
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 const theme = createMuiTheme({
   palette: {
