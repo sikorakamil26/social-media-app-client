bbf4bf54b0c76070389d05aa44b0f94fd03a4e57
Kamil <sikorakamil26>
2020-05-21 20:04:36 +0200

    added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-
+// Components
+import Navbar from "./components/Navbar";
 // Pages
 import home from "./pages/home";
 import login from "./pages/login";
@@ -11,11 +12,14 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={home} />
-          <Route exact path="/login" component={login} />
-          <Route exact path="/signup" component={signup} />
-        </Switch>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+          </Switch>
+        </div>
       </Router>
     </div>
   );
